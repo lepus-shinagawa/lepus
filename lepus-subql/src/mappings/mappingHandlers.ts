5435c424a8151ea1460dbe8059cfd472de44254f
sunagi <yusuke@FixyZ.local>
2023-02-08 16:41:33 +0900

    add dapps stakeing

--- lepus-subql/src/mappings/mappingHandlers.ts
+++ lepus-subql/src/mappings/mappingHandlers.ts
@@ -1,55 +1,25 @@
-import {
-  SubstrateExtrinsic,
-  SubstrateEvent,
-  SubstrateBlock,
-} from "@subql/types";
-import { StarterEntity, Transfer, Account } from "../types";
-import { Balance } from "@polkadot/types/interfaces";
+import {Option, Struct} from '@polkadot/types-codec';
+import {SubstrateEvent} from "@subql/types";
+import {Balance} from '@polkadot/types/interfaces';
+import {Tvl} from '../types/models';
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  //Create a new starterEntity with ID using block hash
-  let record = new StarterEntity(block.block.header.hash.toString());
-  //Record block number
-  record.field1 = block.block.header.number.toNumber();
+export async function handleNewEraEvent(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [era]}} = event;
+    logger.info("New era! ${era}");
+    const result = await api.query.dappsStaking.generalEraInfo<Option<StakingEraInfo>>(era);
+    const tvl = result.unwrap().locked;
+
+  const record = new Tvl(era.toString());
+  record.tvl = tvl.toBigInt();
+  record.startedAt = event.block.timestamp;
   await record.save();
 }
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-  // Get data from the event
-  // The balances.transfer event has the following payload \[from, to, value\]
-  // logger.info(JSON.stringify(event));
-
-  const from = event.event.data[0];
-  const to = event.event.data[1];
-  const amount = event.event.data[2];
-
-  // Create the new transfer entity
-  const transfer = new Transfer(
-    `${event.block.block.header.number.toNumber()}-${event.idx}`
-  );
-  transfer.blockNumber = event.block.block.header.number.toBigInt();
-  transfer.from = from.toString();
-  transfer.to = to.toString();
-  transfer.amount = (amount as Balance).toBigInt();
-  await transfer.save();
-
-  let account = await Account.get(from.toString());
-  if (!account) {
-    account = new Account(from.toString());
-    account.totalTransferAmount = transfer.amount;
-  } else {
-    account.totalTransferAmount += transfer.amount;
+interface StakingEraInfo extends Struct {
+  rewards: {
+    stakers: Balance;
+    dapps: Balance;
   }
-  await account.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-  const record = await StarterEntity.get(
-    extrinsic.block.block.header.hash.toString()
-  );
-  //Date type timestamp
-  record.field4 = extrinsic.block.timestamp;
-  //Boolean tyep
-  record.field5 = true;
-  await record.save();
+  staked: Balance;
+  locked: Balance;
 }
