576733878ddbf38398c4bc2d08555e7f5149d5a4
shinji19 <ichihara.shinji19@gmail.com>
2023-02-17 16:26:28 +0000

    lepus-uiエラー修正

--- lepus-ui/src/App.tsx
+++ lepus-ui/src/App.tsx
@@ -1,18 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DailyVolumeLanking from "./pages/DailyVolume";
-import TotalTransferLanking from "./pages/TotalTransfer"
+import TotalTransferLanking from "./pages/TotalTransfer";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<Home />} />
-      </Routes>
-      <Routes>
         <Route path={`/DailyVolume`} element={<DailyVolumeLanking />} />
-      </Routes>
-      <Routes>
         <Route path={`/TotalTransfer`} element={<TotalTransferLanking />} />
       </Routes>
     </BrowserRouter>
